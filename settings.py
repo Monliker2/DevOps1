fcf1899ce865dcd6cb3c7f3dcbf7c7d5685bc801
Evgeny <evgeny.nov.00@gmail.com>
2024-11-28 09:24:28 +0300

    final

--- settings.py
+++ settings.py
@@ -1,3 +1,13 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'myproject',
+]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
